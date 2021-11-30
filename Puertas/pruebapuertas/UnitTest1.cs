9531683405309fb5e896e556b4c53e876033c0b8
Equis-X <hguzmanpujols@gmail.com>
2021-11-29 21:49:32 -0400

    Modificaciones a PUERTAS

--- Puertas/pruebapuertas/UnitTest1.cs
+++ Puertas/pruebapuertas/UnitTest1.cs
@@ -12,7 +12,22 @@ namespace pruebapuertas
         [TestMethod]
         public void TestMethod1()
         {
-            testdoor.Close();
+            Assert.IsTrue((testdoor.Close() == "OK"),"Cierra");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue((testdoor.Open() == "OK"), "Abre");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue((testdoor.CloseAndLock("llave1") == "OK"), "Cierra y bloquea");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue((testdoor.OpenAndUnlock("llave1") == "OK"), "Abre y desbloquea");
         }
     }
 }
--- Puertas/puertas/Class1.cs
+++ Puertas/puertas/Class1.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace puertas
+﻿namespace puertas
 {
     public class Door
     {
